85e01ad445b34a58e24bec572928612378bd9c0e
tanishi109 <hayakawa@goodpatch.com>
2017-09-25 17:37:38 +0900

    init dining_philosophers

---
+++ projects/dining_philosophers/target/debug/dining_philosophers.d
@@ -0,0 +1,1 @@
+/Users/rent-mac-029tanishi/workspace/hello-rust/projects/dining_philosophers/target/debug/dining_philosophers: /Users/rent-mac-029tanishi/workspace/hello-rust/projects/dining_philosophers/src/main.rs
