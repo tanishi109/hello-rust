560eb7e065c15fa1bc919aa5b961ec65db91aa61
tanishi109 <hayakawa@goodpatch.com>
2017-09-26 17:47:43 +0900

    ffi

---
+++ projects/embed/target/debug/libembed.d
@@ -0,0 +1,1 @@
+/Users/rent-mac-029tanishi/workspace/hello-rust/projects/embed/target/debug/libembed.rlib: /Users/rent-mac-029tanishi/workspace/hello-rust/projects/embed/src/lib.rs
---
+++ projects/embed/target/release/libembed.d
@@ -0,0 +1,1 @@
+/Users/rent-mac-029tanishi/workspace/hello-rust/projects/embed/target/release/libembed.dylib: /Users/rent-mac-029tanishi/workspace/hello-rust/projects/embed/src/lib.rs
