c90ff078b04abf0ce8b2b13b695c868a5597f16c
tanishi109 <hayakawa@goodpatch.com>
2017-09-27 19:10:52 +0900

    rust for wasm! https://qiita.com/_likr/items/daf46d6f66bc31cc4810

---
+++ projects/wasm/target/asmjs-unknown-emscripten/debug/wasm.d
@@ -0,0 +1,1 @@
+/Users/rent-mac-029tanishi/workspace/hello-rust/projects/wasm/target/asmjs-unknown-emscripten/debug/wasm.js: /Users/rent-mac-029tanishi/workspace/hello-rust/projects/wasm/src/main.rs
---
+++ projects/wasm/target/wasm32-unknown-emscripten/debug/wasm.d
@@ -0,0 +1,1 @@
+/Users/rent-mac-029tanishi/workspace/hello-rust/projects/wasm/target/wasm32-unknown-emscripten/debug/wasm.js: /Users/rent-mac-029tanishi/workspace/hello-rust/projects/wasm/src/main.rs
